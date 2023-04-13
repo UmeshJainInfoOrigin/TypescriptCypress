2feb6e6a9f6a71987c236ba377e4df544f100e05
Umesh Jani <umesh.jain@infoorigin.com>
2023-04-13 16:36:53 +0530

    iframe implemenation

---
+++ cypress/integration/iFrame/iFrame.feature
@@ -0,0 +1,11 @@
+Feature: Working on iFrames
+
+    C1~S1~A1~F~
+
+@healthcheck
+Scenario: iFrames handling
+Given Actor on iframe page
+Then Actor click 'mentorship' and validate
+
+Scenario: iFramews on w3school
+Then Actor visited w3schools
---
+++ cypress/integration/iFrame/iFrame.spec.ts
@@ -0,0 +1,29 @@
+/// <reference types="Cypress" />
+/// <reference types="cypress-iframe" />
+import 'cypress-iframe'
+import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+
+Given('Actor on iframe page', ()=> {
+cy.visit('https://rahulshettyacademy.com/AutomationPractice/')
+
+})
+
+Then("Actor click {string} and validate", (desireLink:string) =>{
+    cy.frameLoaded('#courses-iframe')
+    cy.iframe().find(`a[href*=${desireLink}]`).eq(0).click()
+    cy.wait(600)
+    cy.iframe().find('h1[class*="pricing-title"]').should('have.length',2)
+    
+})
+
+Then("Actor visited w3schools", () =>{
+    cy.visit("https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_text_readonly2")
+        cy.frameLoaded("iframe#iframeResult[allowfullscreen='true']")
+        cy.iframe().find('#myText').type('hello')
+        cy.iframe().find("button").contains("Try it").click()
+        cy.iframe().find('#myText').should('have.attr', 'readonly')
+})
+ 
+
+ 
+    
